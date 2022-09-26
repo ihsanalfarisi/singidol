eccbf74c035b42f976de2b3a25dce2210c0b4846
Ihsan <muhammad.ihsan06@ui.ac.id>
2022-09-26 22:21:22 +0700

    'first'

---
+++ src/test/java/apap/tugasindividu/singidol/SingidolApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugasindividu.singidol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingidolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
